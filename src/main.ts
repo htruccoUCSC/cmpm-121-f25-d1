8d83cfdf831d7e682cac24d3983aec00eadfbaa9
htruccoUCSC <htrucco@ucsc.edu>
2025-10-09 22:00:41 +0000

    Step 5 Complete, added the ability to purchase an auto clicker

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,14 @@ let counter: number = 0;
 document.body.innerHTML = `
   <div>Bisons: <span id="counter">0</span></div>
   <button id="increment">🦬</button>
+  <button id="buyAutoClicker" disabled>Purchase Auto Clicker</button>
 `;
 
-const button = document.getElementById("increment")!;
+const button = document.getElementById("increment")! as HTMLButtonElement;
 const counterElement = document.getElementById("counter")!;
+const buyAutoClickerButton = document.getElementById(
+  "buyAutoClicker",
+)! as HTMLButtonElement;
 
 function increaseCounter(value: number) {
   counter += value;
@@ -19,13 +23,23 @@ button.addEventListener("click", () => {
   increaseCounter(1);
 });
 
-const growthRate = 1000;
+let growthRate = 0;
+
+buyAutoClickerButton.addEventListener("click", () => {
+  growthRate += 1;
+  counter -= 10;
+});
 
 let prev = performance.now();
 requestAnimationFrame(counterCheck);
 
 function counterCheck(timestamp: number) {
-  increaseCounter((timestamp - prev) / growthRate);
+  increaseCounter(growthRate ? (timestamp - prev) / (1000 / growthRate) : 0);
   prev = performance.now();
+  if (counter >= 10) {
+    buyAutoClickerButton.disabled = false;
+  } else {
+    buyAutoClickerButton.disabled = true;
+  }
   requestAnimationFrame(counterCheck);
 }
